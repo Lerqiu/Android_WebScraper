3cbaa736a9f03d162439e49c444afdf3de6c8701
Lerqiu <mcwk810@gmail.com>
2020-06-30 20:31:39 +0200

    Poprawa przewijania book info oraz dodanie możliwość kontynuacji

--- app/src/main/java/com/example/webscraper/Info_of_novel.kt
+++ app/src/main/java/com/example/webscraper/Info_of_novel.kt
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment
 import com.example.listAdapters.CustomExpandableListAdapter
 import com.example.listAdapters.InfoAboutNovelCustomAdapter
 import com.google.android.flexbox.FlexboxLayout
+import kotlinx.android.synthetic.main.info_about_novels_layout.*
 import java.lang.Exception
 
 
@@ -31,17 +32,67 @@ class Info_of_novel(mainActivity: MainActivity, webSiteData: WebSiteData) : Frag
     ): View? {
         val view = inflater.inflate(R.layout.info_about_novels_layout, container, false)
 
+        setShowHideArror(view)
+        setTitle(view)
+        setAuthor(view)
+        setTags(view)
+        setLinkText(view)
+        setCopyButton(view)
+        setBrowserButton(view)
+        setLastReaded(view)
+        setExpedabeleList(view)
+
+        return view
+    }
+
+    fun setShowHideArror(view: View) {
+        val arrow = view.findViewById<ImageView>(R.id.showHide)
+        val viewPanel = view.findViewById<ScrollView>(R.id.scrollView)
+
+        arrow.setOnClickListener { V ->
+            when (viewPanel.visibility) {
+                View.GONE -> {
+                    arrow.setImageResource(R.drawable.ic_arrow_up)
+                    viewPanel.visibility = View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    arrow.setImageResource(R.drawable.ic_arrow_down)
+                    viewPanel.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    fun setTitle(view: View) {
         view.findViewById<TextView>(R.id.title).text = webSiteData.Title
+    }
+
+    fun setAuthor(view: View) {
         view.findViewById<TextView>(R.id.author).text = webSiteData.Author
+    }
+
+    fun setTags(view: View) {
         val tags = view.findViewById<FlexboxLayout>(R.id.tags)
-        for (i in webSiteData.genres) {
+
+        if (webSiteData.genres.size == 0) {
             val textV = TextView(mainActivity)
-            textV.setText(i)
+            textV.setText("N/A")
             tags.addView(textV)
+        } else {
+            for (i in webSiteData.genres) {
+                val textV = TextView(mainActivity)
+                textV.setText(i)
+                tags.addView(textV)
+            }
         }
 
-        view.findViewById<TextView>(R.id.link).text = webSiteData.link
+    }
 
+    fun setLinkText(view: View) {
+        view.findViewById<TextView>(R.id.link).text = webSiteData.link
+    }
+
+    fun setCopyButton(view: View) {
         val clipboard = mainActivity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         view.findViewById<ImageButton>(R.id.copy)
             .setOnClickListener(View.OnClickListener { view ->
@@ -54,15 +105,33 @@ class Info_of_novel(mainActivity: MainActivity, webSiteData: WebSiteData) : Frag
                     Toast.LENGTH_SHORT
                 ).show()
             })
+    }
 
-
+    fun setBrowserButton(view: View) {
         view.findViewById<ImageButton>(R.id.open_browser)
             .setOnClickListener(View.OnClickListener { view ->
                 val intent = Intent(Intent.ACTION_VIEW)
                 intent.data = Uri.parse(webSiteData.link)
                 mainActivity.startActivity(intent)
             })
+    }
 
+    fun setLastReaded(view: View){
+        var chapter_text = webSiteData.lastReadedChapter.Name
+        if (chapter_text.length == 0)
+            chapter_text =  webSiteData.lastReadedChapter.Number
+
+        view.findViewById<TextView>(R.id.lastReaded).text = chapter_text
+
+        view.findViewById<ImageButton>(R.id.lastReaded_browserButton)
+            .setOnClickListener(View.OnClickListener { view ->
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(webSiteData.lastReadedChapter.link)
+                mainActivity.startActivity(intent)
+            })
+    }
+
+    fun setExpedabeleList(view: View) {
         expandableListView = view.findViewById(R.id.list_view)
         try {
             if (expandableListView != null) {
@@ -76,7 +145,5 @@ class Info_of_novel(mainActivity: MainActivity, webSiteData: WebSiteData) : Frag
         } catch (e: Exception) {
             println(e)
         }
-
-        return view
     }
 }
